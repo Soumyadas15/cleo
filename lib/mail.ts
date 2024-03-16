5c3435465818ab650d4b0ce576cf9206bd0593cf
Saumyapratim Das <saumyadas2017@gmail.com>
2024-03-17 00:25:53 +0530

    79. Final push

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,15 +1,13 @@
-// Change your mail.ts file
-
 import nodemailer from "nodemailer";
-import fs from "fs/promises";
 import { Audit, Project } from "@prisma/client";
 import { format } from 'date-fns';
+
 /**
  * Sends an email using the provided email address.
  * @param email the email address to send the email to
  * @param auditDetails the details of the newly created audit
  * @param clientName the name of the client
-*/
+ */
 
 export const sendEmail = async (
     email: string,
@@ -27,8 +25,6 @@ export const sendEmail = async (
         },
     });
 
-    const htmlTemplate = await fs.readFile("email-template.html", "utf8");
-
     const dynamicContent = `
         <p>Date: ${format(new Date(audit.date), "MM dd yyyy")}</p>
         <p>Comments: ${audit.comments}</p>
@@ -38,19 +34,41 @@ export const sendEmail = async (
         <p>Action Items: ${audit.actionItem}</p>
     `;
 
-    const auditLink = `http://localhost:3000/main/projects/${project.id}/audits`
+    const auditLink = `http://localhost:3000/main/projects/${project.id}/audits`;
 
-    const dynamicTemplate = htmlTemplate
-        .replace("[auditAction]", auditAction)
-        .replace("[Stakeholder Name]", clientName)
-        .replace("[Audit Details]", dynamicContent)
-        .replace("[Link]", auditLink);
+    const emailTemplate = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Audit update | Promact Infotech</title>
+        </head>
+        <body>
+            <div style="max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 5px;">
+                <h1 style="font-size: 1.875rem; font-weight: bold; color: #fff;">Hello ${clientName}</h1>
+                <p>Hello there!</p>
+                <p>Please note that ${auditAction}</p>
+
+                <div style="background-color: #fff; padding: 15px; border-radius: 5px;">
+                    ${dynamicContent}
+                </div>
+
+                <p>Link: ${auditLink}</p>
+
+                <p>Thanks and regards</p>
+                <p>Promact Infotech Pvt Ltd.</p>
+            </div>
+        </body>
+        </html>
+    `;
 
     const mailOptions: nodemailer.SendMailOptions = {
         from: process.env.NODEMAILER_EMAIL,
         to: email,
         subject: "Audit Update",
-        html: dynamicTemplate,
+        html: emailTemplate,
     };
 
     await transporter.sendMail(mailOptions);
